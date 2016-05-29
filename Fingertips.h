ef08fc21c73fb0bf576be948f232f146791fffea
Matt Diephouse <matt@diephouse.com>
2016-05-29 12:57:35 -0400

    Add an Xcode project with a dynamic framework
    This adds support for building Fingertips with Carthage.

---
+++ Fingertips.h
@@ -0,0 +1,18 @@
+//
+//  Fingertips.h
+//  Fingertips
+//
+//  Copyright © 2016 Mapbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Fingertips.
+FOUNDATION_EXPORT double FingertipsVersionNumber;
+
+//! Project version string for Fingertips.
+FOUNDATION_EXPORT const unsigned char FingertipsVersionString[];
+
+#import <Fingertips/MPFingerTipWindow.h>
+
+
