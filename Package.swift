de3b058af6c6d645dc00aa3e6341ae7d1dde5a0b
Frank Schlegel <frank.schlegel@digitalmasterpieces.com>
2019-07-26 16:45:42 +0200

    Added Package.swift manifest.

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:4.2
+import PackageDescription
+
+let package = Package(
+    name: "Fingertips",
+    products: [
+        .library(name: "Fingertips", targets: ["Fingertips"]),
+    ],
+    targets: [
+        .target(
+            name: "Fingertips",
+            dependencies: [])
+    ]
+)
